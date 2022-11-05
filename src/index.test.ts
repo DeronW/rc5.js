0c76063e0ca6efc1d100a5fc12490107c4bf453e
delong <w.del@qq.com>
2022-11-05 16:01:28 +0800

    feat: some minor update
    1. update jest, ts-jest, typescript version
    2. add @types/*
    3. update package-lock file version, from 1 to 2
    4. use prettier format source code
    5. fix a test file error, Buffer.from method need number array parameter, even "0xab" is also a kind of a number, but got warning
    6. replace few `let` with `const`
    7.  replace `buffer.slice` with `buffer.subarray` read more: https://nodejs.org/dist/latest-v18.x/docs/api/buffer.html#bufslicestart-end

--- src/index.test.ts
+++ src/index.test.ts
@@ -1,85 +1,83 @@
 import RC5 from "./index";
 
 test("adds 1 + 2 to equal 3", () => {
-    expect(1 + 2).toBe(3);
+  expect(1 + 2).toBe(3);
 });
 
 function parseBuf(s: string): Buffer {
-    // "<Buffer d9 62 60 3f 8d b9 09 9f>"
-    let arr = s
-        .slice(8, s.length - 1)
-        .split(" ")
-        .map((i) => "0x" + i);
-    return Buffer.from(arr);
+  // "<Buffer d9 62 60 3f 8d b9 09 9f>"
+  const arr = s
+    .slice(8, s.length - 1)
+    .split(" ")
+    .map((i) => parseInt(i, 16));
+  return Buffer.from(arr);
 }
 
 test("wrong w/r/b", () => {
-    expect(() => {
-        new RC5(Buffer.alloc(256).toString(), 16, 0);
-    }).toThrow();
+  expect(() => {
+    new RC5(Buffer.alloc(256).toString(), 16, 0);
+  }).toThrow();
 
-    expect(() => {
-        new RC5("", 16, 256);
-    }).toThrow();
+  expect(() => {
+    new RC5("", 16, 256);
+  }).toThrow();
 });
 
 test("w/r/b 16/0/0", () => {
-    let rc5 = new RC5("", 16, 0);
-    expect(rc5.S).toEqual([
-        parseBuf("<Buffer 78 65>"),
-        parseBuf("<Buffer 33 f4>"),
-    ]);
+  const rc5 = new RC5("", 16, 0);
+  expect(rc5.S).toEqual([
+    parseBuf("<Buffer 78 65>"),
+    parseBuf("<Buffer 33 f4>"),
+  ]);
 });
 
 test("w/r/b 32/0/0", () => {
-    let rc5 = new RC5("", 32, 0);
-    expect(rc5.S).toEqual([
-        parseBuf("<Buffer 4d ba 7b 7a>"),
-        parseBuf("<Buffer 1e 1d 11 79>"),
-    ]);
+  const rc5 = new RC5("", 32, 0);
+  expect(rc5.S).toEqual([
+    parseBuf("<Buffer 4d ba 7b 7a>"),
+    parseBuf("<Buffer 1e 1d 11 79>"),
+  ]);
 });
 
 test("w/r/b 64/0/0", () => {
-    let rc5 = new RC5("", 64, 0);
-    expect(rc5.S).toEqual([
-        parseBuf("<Buffer d9 62 60 3f 8d b9 09 9f>"),
-        parseBuf("<Buffer 63 0e 0e d0 73 99 d5 d4>"),
-    ]);
+  const rc5 = new RC5("", 64, 0);
+  expect(rc5.S).toEqual([
+    parseBuf("<Buffer d9 62 60 3f 8d b9 09 9f>"),
+    parseBuf("<Buffer 63 0e 0e d0 73 99 d5 d4>"),
+  ]);
 });
 
 test("fast encryption", () => {
-    let rc5 = new RC5("key", 16, 12),
-        source = "桜",
-        cipher = rc5.encrypt(source),
-        plain = rc5.decrypt(cipher);
-    expect(source).toBe(plain.toString());
+  const rc5 = new RC5("key", 16, 12),
+    source = "桜",
+    cipher = rc5.encrypt(source),
+    plain = rc5.decrypt(cipher);
+  expect(source).toBe(plain.toString());
 });
 
 test("standard(default) encryption, w/r/b 32/12/0", () => {
-    let rc5 = new RC5(),
-        source = "桜",
-        cipher = rc5.encrypt(source),
-        plain = rc5.decrypt(cipher);
-    expect(source).toBe(plain.toString());
+  const rc5 = new RC5(),
+    source = "桜",
+    cipher = rc5.encrypt(source),
+    plain = rc5.decrypt(cipher);
+  expect(plain.toString()).toBe(source);
 });
 
 test("complex encryption, w/r/b 64/255/x", () => {
-    let rc5 = new RC5("shakespeare", 64, 255),
-        source = `
-    Two loves I have of comfort and despair,
-    Which like two spirits do suggest me still:
-    The better angel is a man right fair,
-    The worser spirit a woman color’d ill.
-    `,
-        cipher = rc5.encrypt(source),
-        plain = rc5.decrypt(cipher);
-    expect(source).toBe(plain.toString());
+  const rc5 = new RC5("shakespeare", 64, 255),
+    source = `Two loves I have of comfort and despair,
+Which like two spirits do suggest me still:
+The better angel is a man right fair,
+The worser spirit a woman color’d ill.`,
+    cipher = rc5.encrypt(source),
+    plain = rc5.decrypt(cipher);
+  expect(source).toBe(plain.toString());
 });
 
 test("standard with UTF8 coding", () => {
-    let rc5 = new RC5("😂😂😂"),
-        source = "🐁🐂🐅🐇🐉🐍🐎🐐🐒🐓🐕🐖",
-        cipher = rc5.encrypt(source),
-        plain = rc5.decrypt(cipher);
-    expect(source).toBe(plain.toString());
+  const rc5 = new RC5("😂😂😂"),
+    source = "🐁🐂🐅🐇🐉🐍🐎🐐🐒🐓🐕🐖",
+    cipher = rc5.encrypt(source),
+    plain = rc5.decrypt(cipher);
+  expect(source).toBe(plain.toString());
 });
--- src/index.ts
+++ src/index.ts
@@ -1,42 +1,42 @@
 // Pw, Qw are magic constants
 const Pw = {
-    16: Buffer.from([0xb7, 0xe1]),
-    32: Buffer.from([0xb7, 0xe1, 0x51, 0x63]),
-    64: Buffer.from([0xb7, 0xe1, 0x51, 0x62, 0x8a, 0xed, 0x2a, 0x6b]),
+  16: Buffer.from([0xb7, 0xe1]),
+  32: Buffer.from([0xb7, 0xe1, 0x51, 0x63]),
+  64: Buffer.from([0xb7, 0xe1, 0x51, 0x62, 0x8a, 0xed, 0x2a, 0x6b]),
 };
 
 const Qw = {
-    16: Buffer.from([0x9e, 0x37]),
-    32: Buffer.from([0x9e, 0x37, 0x79, 0xb9]),
-    64: Buffer.from([0x9e, 0x37, 0x79, 0xb9, 0x7f, 0x4a, 0x7c, 0x15]),
+  16: Buffer.from([0x9e, 0x37]),
+  32: Buffer.from([0x9e, 0x37, 0x79, 0xb9]),
+  64: Buffer.from([0x9e, 0x37, 0x79, 0xb9, 0x7f, 0x4a, 0x7c, 0x15]),
 };
 
 // Why don't I use BigInt
 // https://github.com/tc39/proposal-bigint/issues/40
 function add(A: Buffer, B: Buffer): Buffer {
-    let buf = Buffer.alloc(A.length),
-        carry = 0;
-    for (let i = A.length - 1; i >= 0; i--) {
-        let t = A[i] + B[i] + carry;
-        carry = t > 255 ? 1 : 0;
-        buf[i] = t;
-    }
-    return buf;
+  const buf = Buffer.alloc(A.length);
+  let carry = 0;
+  for (let i = A.length - 1; i >= 0; i--) {
+    const t = A[i] + B[i] + carry;
+    carry = t > 255 ? 1 : 0;
+    buf[i] = t;
+  }
+  return buf;
 }
 
 function minus(A: Buffer, B: Buffer): Buffer {
-    let buf = Buffer.alloc(A.length),
-        borrow = 0;
-    for (let i = A.length - 1; i >= 0; i--) {
-        let t = A[i] - B[i] - borrow;
-        borrow = t < 0 ? 1 : 0;
-        buf[i] = t;
-    }
-    return buf;
+  const buf = Buffer.alloc(A.length);
+  let borrow = 0;
+  for (let i = A.length - 1; i >= 0; i--) {
+    const t = A[i] - B[i] - borrow;
+    borrow = t < 0 ? 1 : 0;
+    buf[i] = t;
+  }
+  return buf;
 }
 
 function assign(A: Buffer, B: Buffer): void {
-    for (let i = 0; i < A.length; i++) A[i] = B[i];
+  for (let i = 0; i < A.length; i++) A[i] = B[i];
 }
 
 /*
@@ -44,188 +44,187 @@ function assign(A: Buffer, B: Buffer): void {
  * the reason why does't use Integer same as funciton `add` or `minus`
  */
 function merge(A: Buffer, B: Buffer, fn: (a: number, b: number) => number) {
-    let buf = Buffer.alloc(A.length);
-    for (let i = 0; i < A.length; i++) buf[i] = fn(A[i], B[i]);
-    return buf;
+  const buf = Buffer.alloc(A.length);
+  for (let i = 0; i < A.length; i++) buf[i] = fn(A[i], B[i]);
+  return buf;
 }
 
-let xor = (A: Buffer, B: Buffer): Buffer => merge(A, B, (a, b) => a ^ b);
+const xor = (A: Buffer, B: Buffer): Buffer => merge(A, B, (a, b) => a ^ b);
 
 function mod(B: Buffer, n: number): number {
-    if (B.length == 8) {
-        return ((B.readUInt32BE(0) % n) * 2 ** 32 + B.readUInt32BE(4)) % n;
-    } else if (B.length == 4) {
-        return B.readUInt32BE(0) % n;
-    } else {
-        //  B.length == 2
-        return B.readUInt16BE(0) % n;
-    }
+  if (B.length == 8) {
+    return ((B.readUInt32BE(0) % n) * 2 ** 32 + B.readUInt32BE(4)) % n;
+  } else if (B.length == 4) {
+    return B.readUInt32BE(0) % n;
+  } else {
+    //  B.length is 2
+    return B.readUInt16BE(0) % n;
+  }
 }
 
 function rotl(B: Buffer, n: number): Buffer {
-    let buf = Buffer.alloc(B.length),
-        templ = Buffer.concat([B, B]),
-        index = Math.floor(n / 8),
-        pos = n % 8;
-    for (let i = 0; i < buf.length; i++) {
-        let head = (2 ** (8 - pos) - 1) & templ[index + i],
-            tail = ((2 ** pos - 1) << (8 - pos)) & templ[index + i + 1];
-        buf[i] = (head << pos) + (tail >> (8 - pos));
-    }
-    return buf;
+  const buf = Buffer.alloc(B.length),
+    templ = Buffer.concat([B, B]),
+    index = Math.floor(n / 8),
+    pos = n % 8;
+  for (let i = 0; i < buf.length; i++) {
+    const head = (2 ** (8 - pos) - 1) & templ[index + i],
+      tail = ((2 ** pos - 1) << (8 - pos)) & templ[index + i + 1];
+    buf[i] = (head << pos) + (tail >> (8 - pos));
+  }
+  return buf;
 }
 
 function rotr(B: Buffer, n: number): Buffer {
-    return rotl(B, B.length * 8 - n);
+  return rotl(B, B.length * 8 - n);
 }
 
 function expandL({ K, u, c }: Pick<Params, "K" | "u" | "c">): Array<Buffer> {
-    let L = new Array(c).fill(null),
-        filledK = Buffer.concat([K], c * u);
-    for (let i = 0; i < c; i++)
-        L[i] = filledK.slice(i * u, (i + 1) * u).reverse();
-    return L;
+  const L = new Array(c).fill(null),
+    filledK = Buffer.concat([K], c * u);
+  for (let i = 0; i < c; i++)
+    L[i] = filledK.subarray(i * u, (i + 1) * u).reverse();
+  return L;
 }
 
 function expandS({ w, t }: Pick<Params, "w" | "t">): Array<Buffer> {
-    let P = Pw[w],
-        Q = Qw[w],
-        S = [P];
-    for (let i = 1; i < t; i++) S.push(add(S[i - 1], Q));
-    return S;
+  const P = Pw[w],
+    Q = Qw[w],
+    S = [P];
+  for (let i = 1; i < t; i++) S.push(add(S[i - 1], Q));
+  return S;
 }
 
 function mixin(
-    { w, t, c, u }: Pick<Params, "w" | "t" | "c" | "u">,
-    S: Array<Buffer>,
-    L: Array<Buffer>
+  { w, t, c, u }: Pick<Params, "w" | "t" | "c" | "u">,
+  S: Array<Buffer>,
+  L: Array<Buffer>
 ) {
-    let count = Math.max(c, t) * 3,
-        A = Buffer.alloc(u),
-        B = Buffer.alloc(u);
-    for (let k = 0, i = 0, j = 0; k < count; k++) {
-        A = S[i] = rotl(add(S[i], add(A, B)), 3);
-        B = L[j] = rotl(add(L[j], add(A, B)), mod(add(A, B), w));
-        i = (i + 1) % t;
-        j = (j + 1) % c;
-    }
-    return S;
+  const count = Math.max(c, t) * 3;
+  let A = Buffer.alloc(u),
+    B = Buffer.alloc(u);
+  for (let k = 0, i = 0, j = 0; k < count; k++) {
+    A = S[i] = rotl(add(S[i], add(A, B)), 3);
+    B = L[j] = rotl(add(L[j], add(A, B)), mod(add(A, B), w));
+    i = (i + 1) % t;
+    j = (j + 1) % c;
+  }
+  return S;
 }
 
 interface Cryption {
-    args: Pick<Params, "r" | "w">;
-    S: Array<Buffer>;
-    A: Buffer;
-    B: Buffer;
+  args: Pick<Params, "r" | "w">;
+  S: Array<Buffer>;
+  A: Buffer;
+  B: Buffer;
 }
 
 function encryption({ args: { r, w }, S, A, B }: Cryption): [Buffer, Buffer] {
-    A = add(A, S[0]);
-    B = add(B, S[1]);
-    for (let i = 1; i <= r; i++) {
-        A = add(rotl(xor(A, B), mod(B, w)), S[2 * i]);
-        B = add(rotl(xor(B, A), mod(A, w)), S[2 * i + 1]);
-    }
-    return [A.reverse(), B.reverse()];
+  A = add(A, S[0]);
+  B = add(B, S[1]);
+  for (let i = 1; i <= r; i++) {
+    A = add(rotl(xor(A, B), mod(B, w)), S[2 * i]);
+    B = add(rotl(xor(B, A), mod(A, w)), S[2 * i + 1]);
+  }
+  return [A.reverse(), B.reverse()];
 }
 
 function decryption({ args: { r, w }, S, A, B }: Cryption): [Buffer, Buffer] {
-    for (let i = r; i > 0; i--) {
-        B = xor(rotr(minus(B, S[2 * i + 1]), mod(A, w)), A);
-        A = xor(rotr(minus(A, S[2 * i]), mod(B, w)), B);
-    }
-    B = minus(B, S[1]);
-    A = minus(A, S[0]);
-    return [A.reverse(), B.reverse()];
+  for (let i = r; i > 0; i--) {
+    B = xor(rotr(minus(B, S[2 * i + 1]), mod(A, w)), A);
+    A = xor(rotr(minus(A, S[2 * i]), mod(B, w)), B);
+  }
+  B = minus(B, S[1]);
+  A = minus(A, S[0]);
+  return [A.reverse(), B.reverse()];
 }
 
 type TypedW = 16 | 32 | 64;
 type Source = string | Buffer;
 
 interface Params {
-    K: Buffer;
-    u: number;
-    c: number;
-    w: TypedW;
-    r: number;
-    b: number;
-    t: number;
+  K: Buffer;
+  u: number;
+  c: number;
+  w: TypedW;
+  r: number;
+  b: number;
+  t: number;
 }
 
 type ControlBlock = Readonly<Pick<Params, "w" | "r" | "b" | "K">>;
 
 class RC5 {
-    private cb: ControlBlock;
-    private fullParams: Params;
-    S: Array<Buffer>;
+  private cb: ControlBlock;
+  private fullParams: Params;
+  S: Array<Buffer>;
 
-    constructor(key = "", w: TypedW = 32, r = 12) {
-        let K = Buffer.from(key),
-            b = K.length; // number of bytes in key
+  constructor(key = "", w: TypedW = 32, r = 12) {
+    const K = Buffer.from(key),
+      b = K.length; // number of bytes in key
 
-        this.cb = { w, r, b, K };
-        if (r > 255)
-            throw new Error(`Parameter r must be less than 256, got ${r}`);
-        if (b > 255)
-            throw new Error(
-                `Secret key is too long. must less than 255 bytes, got ${b}`
-            );
+    this.cb = { w, r, b, K };
+    if (r > 255) throw new Error(`Parameter r must be less than 256, got ${r}`);
+    if (b > 255)
+      throw new Error(
+        `Secret key is too long. must less than 255 bytes, got ${b}`
+      );
 
-        this.S = [];
-        this.fullParams = this.initParams();
-        this.expand();
+    this.S = [];
+    this.fullParams = this.initParams();
+    this.expand();
+  }
+
+  private initParams() {
+    const { w, r, b, K } = this.cb,
+      t = 2 * (r + 1),
+      u = w / 8,
+      c = Math.ceil(b / u) || 1;
+    return { w, r, b, K, t, u, c };
+  }
+
+  private expand(): void {
+    const L: Array<Buffer> = expandL(this.fullParams);
+    const S: Array<Buffer> = expandS(this.fullParams);
+    mixin(this.fullParams, S, L);
+    this.S = S;
+  }
+
+  private parseBuffer(s: Source): Buffer {
+    const buf = Buffer.from(s),
+      { u } = this.fullParams,
+      u2 = u * 2,
+      gap = (u2 - (buf.length % u2)) % u2;
+    if (gap) return Buffer.concat([buf], buf.length + gap);
+    return buf;
+  }
+
+  private process(
+    source: Source,
+    handle: (args: Cryption) => [Buffer, Buffer]
+  ): Buffer {
+    const buf = this.parseBuffer(source),
+      { u, r, w } = this.fullParams;
+    for (let i = 0; i < buf.length; i += 2 * u) {
+      const A = buf.subarray(i, i + u).reverse(),
+        B = buf.subarray(i + u, i + u * 2).reverse();
+      const [newA, newB] = handle({ args: { r, w }, S: this.S, A, B });
+      assign(A, newA);
+      assign(B, newB);
     }
+    return buf;
+  }
 
-    private initParams() {
-        let { w, r, b, K } = this.cb,
-            t = 2 * (r + 1),
-            u = w / 8,
-            c = Math.ceil(b / u) || 1;
-        return { w, r, b, K, t, u, c };
-    }
+  encrypt(plain: string | Buffer): Buffer {
+    return this.process(plain, encryption);
+  }
 
-    private expand(): void {
-        let L: Array<Buffer> = expandL(this.fullParams);
-        let S: Array<Buffer> = expandS(this.fullParams);
-        mixin(this.fullParams, S, L);
-        this.S = S;
-    }
-
-    private parseBuffer(s: Source): Buffer {
-        let buf = Buffer.from(s),
-            { u } = this.fullParams,
-            u2 = u * 2,
-            gap = (u2 - (buf.length % u2)) % u2;
-        if (gap) buf = Buffer.concat([buf], buf.length + gap);
-        return buf;
-    }
-
-    private process(
-        source: Source,
-        handle: (args: Cryption) => [Buffer, Buffer]
-    ): Buffer {
-        let buf = this.parseBuffer(source),
-            { u, r, w } = this.fullParams;
-        for (let i = 0; i < buf.length; i += 2 * u) {
-            let A = buf.slice(i, i + u).reverse(),
-                B = buf.slice(i + u, i + u * 2).reverse();
-            let [newA, newB] = handle({ args: { r, w }, S: this.S, A, B });
-            assign(A, newA);
-            assign(B, newB);
-        }
-        return buf;
-    }
-
-    encrypt(plain: string | Buffer): Buffer {
-        return this.process(plain, encryption);
-    }
-
-    decrypt(cipher: string | Buffer): Buffer {
-        let filled = this.process(cipher, decryption),
-            pos = filled.length - 1;
-        while (filled[pos] == 0) pos--;
-        return filled.slice(0, pos + 1);
-    }
+  decrypt(cipher: string | Buffer): Buffer {
+    const filled = this.process(cipher, decryption);
+    let pos = filled.length - 1;
+    while (filled[pos] == 0) pos--;
+    return filled.subarray(0, pos + 1);
+  }
 }
 
 export default RC5;
